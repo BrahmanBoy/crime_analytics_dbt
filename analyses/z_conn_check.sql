3f449e8aa0e03c462e276dab504d7472a0531f04
Ankit Sharma <Ankit18293@gmail.com>
2025-10-16 20:38:48 +0000

    checked

--- analyses/z_conn_check.sql
+++
@@ -1,6 +0,0 @@
-select 
-    current_user()     as dbt_user,
-    current_role()     as dbt_role,
-    current_database() as current_db,
-    current_schema()   as current_schema,
-    current_warehouse() as dbt_warehouse;
--- models/zz_conn_check.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-select 
-    current_user()      as dbt_user,
-    current_role()      as dbt_role,
-    current_database()  as current_db,
-    current_schema()    as current_schema,
-    current_warehouse() as dbt_warehouse;
